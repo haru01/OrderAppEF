99910cf64428959f9a746e3ec755a5b2bd17a5a6
“Eiji <eiji.ienaga@gmail.com>
2024-12-18 08:55:56 +0900

    Delete sql

--- sql/V2__AddNewColumn.sql
+++
@@ -1,4 +0,0 @@
-﻿ALTER TABLE "Orders" ADD "CustomerId" INTEGER NOT NULL DEFAULT 0;
-
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('20241217134707_AddNewColumn', '9.0.0');
